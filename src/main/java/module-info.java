a090624a7fb436b16c5db30373d471ea9d0b72ec
HoseinA05 <asf.hosein@gmail.com>
2024-06-19 11:11:15 +0330

    Designed Basic Template for Video Page

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module sbu.cs.youtube {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens youtube to javafx.fxml;
---
+++ src/main/java/youtube/VideopageController.java
@@ -0,0 +1,118 @@
+package youtube;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.SVGPath;
+
+public class VideopageController {
+
+    @FXML
+    public VBox leftPanelMin;
+    @FXML
+    public MediaView mediaView;
+    @FXML
+    public ImageView img;
+    @FXML
+    public ImageView profImg;
+    @FXML
+    public ImageView userProfImg;
+    @FXML
+    public ImageView otherUserProfImg;
+    @FXML
+    public ImageView repliedUserProfImg;
+    @FXML
+    public ScrollPane rightScrollpane;
+    @FXML
+    public ScrollPane centerScrollpane;
+    @FXML
+    public Button repliesBtn;
+    @FXML
+    public SVGPath repliesSvg;
+    private final String darkTheme = getClass().getResource("styles/dark-theme.css").toExternalForm();
+    private final String lightTheme = getClass().getResource("styles/light-theme.css").toExternalForm();
+
+    public void initialize() {
+        // Don't show left panel by default in video page.
+        leftPanelMin.setVisible(false);
+        leftPanelMin.setManaged(false);
+
+        // Bind the right and center scrollpanes
+        centerScrollpane.vvalueProperty().bindBidirectional(rightScrollpane.vvalueProperty());
+        centerScrollpane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        // Make Recommended Video Image Rounded.
+        Rectangle clip = new Rectangle(img.getFitWidth(), img.getFitHeight());
+        clip.setArcWidth(20);
+        clip.setArcHeight(20);
+        img.setClip(clip);
+        // Make Profile Image of video creator Rounded.
+        Rectangle clip3 = new Rectangle(profImg.getFitWidth(), profImg.getFitHeight());
+        clip3.setArcWidth(50);
+        clip3.setArcHeight(50);
+        profImg.setClip(clip3);
+        // Make Profile Image of Add a Comment Rounded.
+        Rectangle clip4 = new Rectangle(userProfImg.getFitWidth(), userProfImg.getFitHeight());
+        clip4.setArcWidth(50);
+        clip4.setArcHeight(50);
+        userProfImg.setClip(clip4);
+        // Make Profile Image of Other peoples Comment Rounded.
+        Rectangle clip5 = new Rectangle(otherUserProfImg.getFitWidth(), otherUserProfImg.getFitHeight());
+        clip5.setArcWidth(50);
+        clip5.setArcHeight(50);
+        otherUserProfImg.setClip(clip5);
+        // Make Profile Image of replied Comment Rounded.
+        Rectangle clip6 = new Rectangle(repliedUserProfImg.getFitWidth(), repliedUserProfImg.getFitHeight());
+        clip6.setArcWidth(50);
+        clip6.setArcHeight(50);
+        repliedUserProfImg.setClip(clip6);
+
+        // Set up the MediaView
+        Media media = new Media(getClass().getResource("test.mp4").toExternalForm()); // Replace with your video file
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+
+        // Play the video (for demonstration purposes)
+        mediaPlayer.setAutoPlay(false);
+
+        // Making Video Rounded
+        Rectangle clip2 = new Rectangle(mediaView.getFitWidth(), mediaView.getFitHeight());
+        clip2.setArcWidth(20);
+        clip2.setArcHeight(20);
+        mediaView.setClip(clip2);
+
+
+    }
+
+    @FXML
+    public void repliesBtnHandler(ActionEvent actionEvent) {
+        String downArrowSvg = "m18 9.28-6.35 6.35-6.37-6.35.72-.71 5.64 5.65 5.65-5.65z";
+        if(repliesSvg.getContent().equals(downArrowSvg))
+            repliesSvg.setContent("M18.4 14.6 12 8.3l-6.4 6.3.8.8L12 9.7l5.6 5.7z");
+        else repliesSvg.setContent(downArrowSvg);
+    }
+    @FXML
+    public void leftPanelBtnHandler(ActionEvent actionEvent) {
+        leftPanelMin.setVisible(!leftPanelMin.isVisible());
+        leftPanelMin.setManaged(!leftPanelMin.isManaged());
+    }
+
+    @FXML
+    public void toggleTheme(ActionEvent actionEvent) {
+        if (leftPanelMin.getScene().getStylesheets().contains(darkTheme)){
+            leftPanelMin.getScene().getStylesheets().remove(darkTheme);
+            leftPanelMin.getScene().getStylesheets().add(lightTheme);
+        }else {
+            leftPanelMin.getScene().getStylesheets().remove(lightTheme);
+            leftPanelMin.getScene().getStylesheets().add(darkTheme);
+        }
+    }
+
+}
